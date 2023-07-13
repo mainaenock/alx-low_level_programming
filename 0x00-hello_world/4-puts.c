c9de8e4ffc872e7dbca899085b41774e13af116d
mainaenock <mainaenock10@gmail.com>
2023-07-13 11:57:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-/**
- * main - entry point
- *
- * return: 0 (success)
+
+/**                                                                                 
+ * main - Entry point                                                               
+ *                                                                                  
+ * Description: 'the program's description'                                         
+ *                                                                                  
+ * Return: Always 0 (Success)                                                       
  */
+
 int main(void)
 {
-
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 
 return (0);
 
