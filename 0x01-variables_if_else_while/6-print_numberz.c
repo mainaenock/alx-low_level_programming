f881a98131f6e4d9d83c5328d7fe51fdecc6cf7b
mainaenock <mainaenock10@gmail.com>
2023-07-16 14:44:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+putchar(num + '0');
+num++;
+}
+putchar('\n');
+return (0);
+}
