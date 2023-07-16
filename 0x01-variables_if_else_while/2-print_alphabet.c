88434c98fb5f3a5e8a5d75e2bb0aa03af4149533
mainaenock <mainaenock10@gmail.com>
2023-07-16 13:02:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char lt = 'a';
+while (lt <= 'z')
+{putchar (lt);
+lt++;
+}
+putchar ('\n');
+return (0);
+}
