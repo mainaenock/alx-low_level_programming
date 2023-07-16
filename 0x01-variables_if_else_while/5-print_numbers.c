3d38650bbaf30c70cda3f071667e0ccf50885f75
mainaenock <mainaenock10@gmail.com>
2023-07-16 14:24:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+printf("%d\n", num);
+num++;
+}
+return (0);
+}
