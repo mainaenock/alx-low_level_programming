c1afda9f0cd22e94cf72b6ba2b876a1c4f094e70
mainaenock <mainaenock10@gmail.com>
2023-07-17 01:06:38 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,17 @@
  */
 int main(void)
 {
-int num = 1;
-putchar ('0');
+int num = 0;
 while (num <= 9)
 {
+	if (num != 0 && num <= 9)
+	{
 	putchar (',');
 	putchar (' ');
+	}
 	putchar (num + '0');
 	num++;
 }
+putchar ('\n');
 return (0);
 }
