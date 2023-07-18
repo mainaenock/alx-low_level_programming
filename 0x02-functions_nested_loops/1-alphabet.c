5314d9589170055582265bc215a7bf122e493d93
mainaenock <mainaenock10@gmail.com>
2023-07-18 10:05:18 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char a = 'a';
+    while (a <= 'z')
+    {
+    printf("%c",a);
+    a++;
+    }
+    printf("\n");
+    return (0);
+}
