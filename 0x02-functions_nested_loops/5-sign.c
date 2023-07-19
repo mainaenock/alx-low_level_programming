c7d69eebe839f285f5c4a79f6b2692d7ccc54a69
mainaenock <mainaenock10@gmail.com>
2023-07-19 12:44:10 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the code
+ *
+ * @n: the integer
+ *
+ * Return: 0  and -1
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+	_putchar (43);
+	return (1);
+}
+else if (n < 0)
+{
+	_putchar (48);
+	return (-1);
+}
+else
+{
+	_putchar (45);
+	return (0);
+}
+}
