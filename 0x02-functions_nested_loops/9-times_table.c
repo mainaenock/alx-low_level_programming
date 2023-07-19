876bc251b1703e4833bbf3811bcced9cfae28024
mainaenock <mainaenock10@gmail.com>
2023-07-19 14:59:27 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - checker
+ *
+ * return: ..
+ */
+void times_table(void)
+{
+int num, r, c, temp;
+for (r = 0; r <= 9; r++)
+{
+	for (c = 0; c <= 9; c++)
+	{
+		num = r * c;
+		temp = num;
+		if (num > 9)
+		{
+		_putchar (num + '0');
+		temp = num % 10;
+		}
+		_putchar (temp + '0');
+		_putchar (',');
+		_putchar (' ');
+	}
+	_putchar ('\n');
+}
+}
