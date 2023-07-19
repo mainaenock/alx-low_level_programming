b4364ffacc465b355dd963f90a6ebe52861aad87
mainaenock <mainaenock10@gmail.com>
2023-07-19 11:54:20 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - the main
+ *
+ * @c: main c _islower
+ *
+ * Return: 0 and 1
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+	return (1);
+}
+	return (0);
+}
