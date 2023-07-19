387a39f1d32bf4d6550cd5ab0232bccdf7bdf018
mainaenock <mainaenock10@gmail.com>
2023-07-19 13:09:08 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - the return value
+ *
+ * @c: the int
+ *
+ * Return: 0
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+
+int num;
+num = c * -1;
+return (num);
+}
+return (c);
+}
