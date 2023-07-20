3fed05e96fb65cb85d22a8b17121bddaf924cea3
mainaenock <mainaenock10@gmail.com>
2023-07-20 13:54:46 -0700

    more no

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - checker
+ *
+ * Return: ..
+ */
+void more_numbers(void)
+{
+int r, c;
+for (r = 0; r < 10; r++)
+{
+	for (c = 0; c <= 14; c++)
+	{
+		if (c > 9)
+		{
+		_putchar ('1');
+		}
+		_putchar (c % 10 + '0');
+		
+	}
+	_putchar ('\n');
+}
+}
