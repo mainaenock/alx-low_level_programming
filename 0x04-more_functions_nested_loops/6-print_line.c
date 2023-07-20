e03205c12899a9dffef4184d01b60766ec3ae527
mainaenock <mainaenock10@gmail.com>
2023-07-20 14:35:05 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,18 @@
  */
 void print_line(int n)
 {
-int i;
-for (i = 0; i <= n; i++)
-{
 	if (n <= 0)
 	{
 		_putchar ('\n');
+		return;
 	}
 	else
 	{
+		int i;
+		for (i = 1; i <= n; i++)
+		{
 		_putchar ('_');
-	}
+		}
 }
 _putchar ('\n');
 }
