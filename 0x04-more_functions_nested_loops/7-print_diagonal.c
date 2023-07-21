df9eeffbca92724c73b047c48c8d4054bbc5d2c2
mainaenock <mainaenock10@gmail.com>
2023-07-21 11:13:47 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,6 @@ void print_diagonal(int n)
 if (n <= 0)
 {
 	_putchar ('\n');
-	return;
 }
 else
 {
@@ -19,18 +18,12 @@ else
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
-			if (j == i)
-			{
-				_putchar ('/');
-			}
-			else if (j < i)
-			{
-				_putchar (' ');
-			}
+			_putchar (' ');
+		}
+			_putchar ('\\');
 			_putchar ('\n');
 		}
 	}
-}
 }
