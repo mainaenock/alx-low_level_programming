959548002ea6f13fdbd0b13f6e59a78c9c2010bb
mainaenock <mainaenock10@gmail.com>
2023-07-21 11:48:36 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,19 +2,27 @@
 
 /**
  * print_square - the checker
+ * @size: the times
  *
  * Return: ..
  */
 void print_square(int size)
 {
-int i, j;
-
-for (i = 1; i <= size; i++)
-{
-	for (j = 1; j <= size; j++)
+	if (size <= 0)
 	{
-		_putchar ('#');
+		_putchar ('\n');
 	}
-	_putchar ('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar ('#');
+		}
+		_putchar ('\n');
+}
 }
 }
