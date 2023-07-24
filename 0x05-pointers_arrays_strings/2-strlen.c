f375e5c6b62c076cc9819ff46e0249650fb51542
mainaenock <mainaenock10@gmail.com>
2023-07-24 14:34:10 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - entry
+ * @s: times
+ *
+ * Return: ..
+ */
+
+int _strlen(char *s)
+{
+
+int li;
+
+li = 0;
+while (*s != '\0')
+	{
+		li++;
+		s++;
+	}
+
+	return (li);
+}
