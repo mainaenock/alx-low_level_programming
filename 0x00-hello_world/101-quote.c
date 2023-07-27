dae62f6e54016c5643cb14d2cc31f15a387b2bca
mainaenock <mainaenock10@gmail.com>
2023-07-27 14:48:33 -0700

    0-strcat.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stderr.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+and that piece of art is useful" - Dora Korpar, 2015-10-19
+return (1);
+}
