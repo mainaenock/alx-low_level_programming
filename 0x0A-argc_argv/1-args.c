b9461cad289d18eade7a1c82242a22c33d369500
mainaenock <mainaenock10@gmail.com>
2023-08-04 13:56:10 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: vector counter
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
