156beb2ecdbe09633a1de10f4231e3b09cf636e5
mainaenock <mainaenock10@gmail.com>
2023-08-05 01:05:34 -0700

    addd

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcat - prototype
+ * @dest: dest
+ * @scr: scr
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{    
+	char ptr[];
+
+	strcat (dest, src);
+
+return (dest);
+}
