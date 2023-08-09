fef3af8696a3574bc930aee0b59d7c6a0d6f8379
mainaenock <mainaenock10@gmail.com>
2023-08-09 16:29:16 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - prototype
+ * @width: width input
+ * @height: height input
+ * Return: 2
+ */
+int **alloc_grid(int width, int height)
+{
+	int **z;
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	z = malloc(sizeof(int *) * height);
+
+	if (z == NULL)
+		return (NULL);
+
+	for (x = 0; x < height; x++)
+	{
+		z[x] = malloc(sizeof(int) * width);
+
+		if (z[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(z[x]);
+
+			free(z);
+			return (NULL);
+		}
+	}
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			z[x][y] = 0;
+	}
+
+	return (z);
+}
+
+
