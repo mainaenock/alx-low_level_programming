4e79917dce568fadf231d5ded000b066feea1fb0
mainaenock <mainaenock10@gmail.com>
2023-08-08 18:02:00 -0700

    0-create_array.c

---
+++ 0-create_array.c/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - prototype
+ * @size: input
+ * @c: input
+ * Description: function that creates an array of chars
+ *
+ * Return: NULL and 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+char *ar;
+unsigned int i;
+
+ar = malloc(sizeof(char) * size);
+
+if (size == 0 || ar == NULL)
+{
+	return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+	ar[i] = c;
+}
+return (ar);
+}
+
