1a26d33a9ac4b49c05b0fd166aadb6505a2938df
mainaenock <mainaenock10@gmail.com>
2023-10-25 10:24:47 -0400

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,2 +1,20 @@
 #include "main.h"
+#include <string.h>
 
+/**
+ * _strncpy - function
+ *
+ * @dest: destination
+ * @src: source
+ * @n: number of characters being copied
+ *
+ * Description: strncpy
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+
+	return (dest);
+}
