b12bc4b80163ee4d56063ad2cddc1df67e8383dd
mainaenock <mainaenock10@gmail.com>
2023-10-25 10:34:00 -0400

    strncmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,2 +1,20 @@
 #include "main.h"
+#include <string.h>
 
+/**
+ * _strcmp - function
+ * @s1: 1st string
+ * @s2: 2nd string
+ *
+ * Description: compares 2 strings
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int value;
+
+	value = strcmp(s1, s2);
+
+	return (value);
+}
