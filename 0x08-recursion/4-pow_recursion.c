98f4f2015fdf8a1d179c1d3ac36a7582dca85d37
mainaenock <mainaenock10@gmail.com>
2023-10-26 17:58:41 -0400

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,10 +16,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else
+	else if(y == 0)
 	{
-
-	return (pow(x, y));
+		return (1);
 	}
+	
+
+	return x * _pow_recursion(x, y - 1);
+	
 
 }
