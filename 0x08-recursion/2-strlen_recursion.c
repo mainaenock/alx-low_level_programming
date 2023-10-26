83afdbfe06e104a22be75ebd4ab86fef6b5e07c7
mainaenock <mainaenock10@gmail.com>
2023-10-26 12:54:33 -0400

    rec 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - function
+ *
+ * @s: string
+ * Description: returns the length of the string
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = strlen(s);
+
+	return (i);
+}
