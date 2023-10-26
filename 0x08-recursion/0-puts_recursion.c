c43ebc757e4803c21aff70e2ba51f107617bddfe
mainaenock <mainaenock10@gmail.com>
2023-10-26 12:28:49 -0400

    rec 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - the function
+ * @s: the string
+ *
+ * Description: function that prints a string followed by a new line
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts (s);
+	_putchar ('\n');
+
+}
