9819d248e823143c693e38071641553e5a89cf13
mainaenock <mainaenock10@gmail.com>
2023-10-31 22:24:12 -0400

    malloc 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,10 @@ char *str_concat(char *s1, char *s2)
 {
 	char *con;
 
-
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
 
 	con = malloc(strlen(s1) + strlen(s2) + 1);
 	if (con == NULL)
