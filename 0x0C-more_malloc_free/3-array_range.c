31815070a9f32e168b6bf64fac332f699a3a10f9
mainaenock <mainaenock10@gmail.com>
2023-11-02 15:18:07 -0400

    more malloc 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -28,9 +28,9 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	for (i = 0; i <= size; i++)
+	for (i = min; i <= max; i++)
 	{
-		ptr[i] = min + i;
+		ptr[i] = i;
 	}
 	return (ptr);
 }
