dfa7e4ac5e6ec996ecf7db271697e8670523d9eb
mainaenock <mainaenock10@gmail.com>
2023-11-04 23:11:20 -0400

    more malloc 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,24 +22,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 	{
 		return (ptr);
-		free(ptr);
 
 	}
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 
-		free(ptr);
 		return (NULL);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 	if (new_size > old_size)
 	{
 		ptr2 = malloc(new_size);
+		if (ptr2 == NULL)
+		{
+			return (NULL);
+		}
 		memcpy(ptr2, ptr, old_size);
 		free(ptr);
 	}
