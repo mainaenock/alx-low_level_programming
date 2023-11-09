459f908c5d744bfab80ca69d4d71dbd1d1e16d47
mainaenock <mainaenock10@gmail.com>
2023-11-09 03:26:56 -0500

    function pointers 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prototype
+ * @array: array of integers
+ * @size: size of the array
+ * @action: function pointer
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		(action)(array[i]);
+	}
+}
