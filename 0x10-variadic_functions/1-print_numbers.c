f726d6afab60dd16650603003013100c2c26440f
mainaenock <mainaenock10@gmail.com>
2023-11-10 11:16:28 -0500

    variadic functions 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,17 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list enock;
 	unsigned int i;
 
-	if (separator == NULL || n == 0)
-	{
-		return;
-	}
-
 	va_start(enock, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(enock, int));
-		if (i != n - 1)
+		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
