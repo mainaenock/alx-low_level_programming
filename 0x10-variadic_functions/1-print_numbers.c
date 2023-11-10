91192f1819de2ba1fb676ca883f3ece1de8532f7
mainaenock <mainaenock10@gmail.com>
2023-11-10 10:14:43 -0500

    variadic functions 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prototype
+ * @separator: string
+ * @n: number of arguments
+ *
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list enock;
+	unsigned int i;
+
+	if (separator == NULL)
+	{
+		return;
+	}
+
+	va_start(enock, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(enock, int));
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	va_end(enock);
+}
