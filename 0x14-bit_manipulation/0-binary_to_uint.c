36392eb45d99fa8f3e70a58dbfb072d94e235673
mainaenock <mainaenock10@gmail.com>
2023-11-23 08:51:15 -0500

    bit 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,11 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, j = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
+
 		for (i = 0; b[i] != '\0'; i++)
 		{
 			if (b[i] != '0' && b[i] != '1')
