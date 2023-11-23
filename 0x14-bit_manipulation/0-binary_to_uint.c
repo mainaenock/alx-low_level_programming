d7ce2ba3685841829769bd0c83c5d85c4109c68d
mainaenock <mainaenock10@gmail.com>
2023-11-23 08:48:54 -0500

    bit 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - prototype of the function
+ * @b: pointer to string containing binary
+ * Description: changes binary to int
+ *
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, j = 0;
+
+		for (i = 0; b[i] != '\0'; i++)
+		{
+			if (b[i] != '0' && b[i] != '1')
+			{
+				return (0);
+			}
+
+			j = j * 2 + (b[i] - '0');
+
+		}
+
+
+
+
+	return (j);
+
+}
+
