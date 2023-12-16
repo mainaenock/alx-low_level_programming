7076b8b0126a4abc597e7df864dc12af9bcd104b
mainaenock <mainaenock10@gmail.com>
2023-12-16 14:43:33 -0500

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - prototype
+ * @n: the long number/binary
+ * @index: the index
+ *
+ * Return: the index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+
+	i = sizeof(unsigned long int) * 8;
+
+	if (index >= i)
+	{
+		return (-1);
+	}
+	else
+	{
+		return ((n >> index) & 1);
+	}
+
+}
