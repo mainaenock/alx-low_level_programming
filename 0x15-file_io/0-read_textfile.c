5ef84d39c49664f855df45a7e16ce3825d068a18
mainaenock <mainaenock10@gmail.com>
2024-02-14 08:27:24 -0500

    read text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -41,7 +41,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	close(fd);
-	printf("%.*s",(int)btread, buffer);
+	printf("%s", buffer);
 	free(buffer);
 	return (btread);
 
