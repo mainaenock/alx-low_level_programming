3bdc656bab633961c43c2b685048e48282373faf
mainaenock <mainaenock10@gmail.com>
2024-02-14 09:41:43 -0500

    create text

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
+	if (access(filename, F_OK) == 0)
+	{
+		fd = open(filename, O_WRONLY | O_TRUNC);
+	}
+	else
+	{
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	}
 	if (fd == -1)
 	{
 		return (-1);
