68367c0145049685c0c672c3110cbad27e8b8200
mainaenock <mainaenock10@gmail.com>
2024-02-16 11:03:32 -0500

    arg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -29,11 +29,8 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			return (1);
 		}
-		else
-		{
-			num = atoi(argv[i]);
-			sum += num;
-		}
+		num = atoi(argv[i]);
+		sum += num;
 	}
 	printf("%d\n", sum);
 	}
