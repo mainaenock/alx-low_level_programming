a932fa69ecb5612be3148127c0d15885d5595313
mainaenock <mainaenock10@gmail.com>
2024-02-16 11:05:39 -0500

    arg

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,14 +15,15 @@ int main(int argc, char *argv[])
 {
 	int sum = 0;
 	int num, i;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 	}
 	else
 	{
-	
-	for(i = 1; i < argc; i++)
+
+	for (i = 1; i < argc; i++)
 	{
 		if (!isdigit(*argv[i]))
 		{
