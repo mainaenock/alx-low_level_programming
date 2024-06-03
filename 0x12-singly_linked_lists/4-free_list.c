27cb4258f1a010a01893795ab9c096c3298af904
mainaenock <mainaenock10@gmail.com>
2024-06-03 13:11:48 -0400

    singly linkd list 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - deletes the list
+ * @head: the head
+ *
+ * Return: none
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
