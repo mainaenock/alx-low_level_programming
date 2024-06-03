4298df638540e10de1766b9a6fde351cc7596bff
mainaenock <mainaenock10@gmail.com>
2024-06-03 11:19:21 -0400

    singly linkd list 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * list_len - function to print the items in the list
+ * @h: the head
+ *
+ * Return: always 0
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *temp;
+
+	temp = h;
+	while (temp != 0)
+	{
+		count ++;
+		temp = temp->next;
+	}
+	return (count);
+}
