b32f5486750ff12d9e5bf80d31a76fe9289024a8
mainaenock <mainaenock10@gmail.com>
2024-06-03 13:43:23 -0400

    singly linkd list 0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) before(void);
+/**
+ * before_main - print a message before the main function is execute.
+ *
+ * Return: nothing.
+ */
+
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
