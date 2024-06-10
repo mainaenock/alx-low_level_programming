982d586748bef0f3b3f8e5b734c1422d54275ae8
mainaenock <mainaenock10.com>
2024-06-10 14:34:06 -0400

    more singly lists 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node
+ * @head: the head
+ *
+ * Return: data of the head node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	temp = *head;
+	*head = temp->next;
+	return (temp->n);
+	free(temp);
+}
