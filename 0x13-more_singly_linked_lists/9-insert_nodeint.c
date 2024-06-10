df213dba25fe712d080588869241e3c42e477f0c
mainaenock <mainaenock10.com>
2024-06-10 15:54:42 -0400

    more singly lists 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			newnode->next = temp->next;
 			temp->next = newnode;
+			return (newnode);
 
 		}
 		count++;
