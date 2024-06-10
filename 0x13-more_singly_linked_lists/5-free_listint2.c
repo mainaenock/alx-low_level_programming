bd322df5a7413432747a8c70b784df2eb88be63c
mainaenock <mainaenock10.com>
2024-06-10 14:22:05 -0400

    more singly lists 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,6 +13,10 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	if (*head == NULL)
+	{
+		return;
+	}
 
 	while (*head != NULL)
 	{
@@ -20,5 +24,4 @@ void free_listint2(listint_t **head)
 		*head = temp->next;
 		free(temp);
 	}
-	*head = NULL;
 }
