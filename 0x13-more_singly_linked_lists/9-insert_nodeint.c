d937b9306f8f411a51163e7f283390e4f1543028
mainaenock <mainaenock10.com>
2024-06-10 16:00:31 -0400

    more singly lists 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,9 +24,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	newnode->n = n;
+	newnode->next = NULL;
 	while (temp != NULL)
 	{
-		if (idx - 1 == count)
+		if (idx == 0)
+		{
+			newnode->next = *head;
+			*head = newnode;
+			return (newnode);
+		}
+		else if (idx - 1 == count)
 		{
 			newnode->next = temp->next;
 			temp->next = newnode;
